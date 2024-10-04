707e5e550ba5a500fbfa27e212605d68bb7b76db
wafanakha <wafanakha15@gmail.com>
2024-10-04 14:37:03 +0700

    add interaction react

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ function Header() {
   return <h1>This is header</h1>;
 }
 function Homepage() {
+  const names = ["whale", "docker", "sanity", "mother"];
+
+  function handleClick() {
+    console.log("pressed");
+  }
   return (
     <div>
       <Header />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Click Here!</button>
     </div>
   );
 }
-root.render(<Header />);
+root.render(<Homepage />);
